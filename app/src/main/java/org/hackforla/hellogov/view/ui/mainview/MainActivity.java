bef6755e340e02e1916cb5b75e6f0e3c38c1ed0c
hendercine <james@hendercine.com>
2018-12-17 19:43:54 -0800

    refactor($java): organize packages

--- app/src/main/java/org/hackforla/hellogov/view/ui/mainview/MainActivity.java
+++ app/src/main/java/org/hackforla/hellogov/view/ui/mainview/MainActivity.java
@@ -1,7 +1,17 @@
-package org.hackforla.hellogov;
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 10/29/18 7:37 PM
+ */
+
+package org.hackforla.hellogov.view.ui.mainview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.hackforla.hellogov.R;
 
 public class MainActivity extends AppCompatActivity {
 
