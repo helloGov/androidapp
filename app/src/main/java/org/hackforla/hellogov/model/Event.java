db8161d3ace6e105e1bdc85de0b489024cea3271
hendercine <james@hendercine.com>
2019-02-04 19:52:21 -0800

    feat($model): add Event model

---
+++ app/src/main/java/org/hackforla/hellogov/model/Event.java
@@ -0,0 +1,22 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 12/17/18 7:58 PM
+ */
+
+package org.hackforla.hellogov.model;
+
+/**
+ * helloGov created by hendercine on 12/17/18.
+ */
+public class Event {
+
+    String mType;
+    String mIp;
+    User mUser;
+
+    String mCampaign;
+
+}
