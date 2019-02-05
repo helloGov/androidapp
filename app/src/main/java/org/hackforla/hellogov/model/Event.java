09a4c8c699e7dc656830782bd0eefde9d4ac2f4c
hendercine <james@hendercine.com>
2019-02-04 20:30:18 -0800

    feat($view): add listItem for activity_main

--- app/src/main/java/org/hackforla/hellogov/model/Event.java
+++ app/src/main/java/org/hackforla/hellogov/model/Event.java
@@ -8,15 +8,30 @@
 
 package org.hackforla.hellogov.model;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 /**
  * helloGov created by hendercine on 12/17/18.
  */
+
+@Entity(tableName = "event_table")
 public class Event {
 
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "_id")
+    private int mId;
+
+    @ColumnInfo(name = "firstName")
     String mType;
+
+    @ColumnInfo(name = "lastName")
     String mIp;
+
+    @ColumnInfo(name = "user")
     User mUser;
 
+    @ColumnInfo(name = "campaign")
     String mCampaign;
 
 }
